8ec05fb16f14052b0f8471fa15b843f7237d79dd
Ed Wu <ed.sh.wu@gmail.com>
2018-12-21 14:27:21 -0800

    complete server routes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,53 @@
 const express = require('express');
 const port = 3000;
+const database = require('../database/database.js')
 
 const app = express();
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + '../client/dist'));
+
+app.get('/rooms/:roomId/reviews', (req, res) => {
+
+    const roomId = req.params.roomId;
+    database.getReviewsByRoomId(roomId, (err, results) => {
+      if(err) {
+        console.log(err);
+        res.sendStatus(501);
+      } else {
+        console.log(results);
+        res.send(results);
+      }
+    })
+})
+
+app.get('/rooms/:roomId/stars', (req, res) => {
+  
+  const roomId = req.params.roomId;
+  database.getAverageStarsByRoomId(roomId, (err, results) => {
+    if(err) {
+      console.log(err);
+      res.sendStatus(501);
+    } else {
+      console.log(results);
+      res.send(results);
+    }
+  })
+})
+
+app.get('/rooms/:roomId/query', (req, res) => {
+
+    const roomId = req.params.roomId;
+    const queryTerm = req.query.queryTerm;
+    database.getReviewsByRoomIdAndQueryTerm(roomId, queryTerm, (err, results) => {
+      if(err) {
+        console.log(err);
+        res.sendStatus(501);
+      } else {
+        console.log(results);
+        res.send(results);
+      }
+    })
+
+})
 
 app.listen(port, () => console.log(`listening on port ${port}`));
