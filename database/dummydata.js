8dae588bc7614cfb2c3bd6c1efa13db4f39b217e
Ed Wu <ed.sh.wu@gmail.com>
2018-12-21 11:40:58 -0800

    standardize naming convention for all schema columns to use underscore _

--- database/dummydata.js
+++ database/dummydata.js
@@ -7,14 +7,14 @@ var NUM_OF_USERS = 300;
 var NUM_OF_ROOMS = 100;
 var NUM_OF_MESSAGES = 3000;
 
-var usersQueryStr = `INSERT INTO users (firstName, lastName, image_photo_path) VALUES (?, ?, ?);`;
+var usersQueryStr = `INSERT INTO users (first_name, last_name, image_photo_path) VALUES (?, ?, ?);`;
 var usersParams = ['firstName', 'lastName', 'image_photo_path'];
 
 var roomsQueryStr = `INSERT INTO rooms (name, host_id) VALUES (?, ?);`;
 var roomsParams = ['name', 'host_id'];
 
-var reviewsQueryStr = `INSERT INTO reviews (reviewer_id, room_id, review_date, review_text, reviewIsEnglish, review_text_eng, hasHostResponse, host_reply_text, stars_accuracy, stars_communication, stars_location, stars_checkin, stars_cleanliness, stars_value) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`;
-var reviewsParams = ['reviewer_id', 'room_id', 'review_date', 'review_text', 'reviewIsEnglish', 'review_text_eng', 'hasHostResponse', 'host_reply_text', 'stars_accuracy', 'stars_communication', 'stars_location', 'stars_checkin', 'stars_cleanliness', 'stars_value'];
+var reviewsQueryStr = `INSERT INTO reviews (reviewer_id, room_id, review_date, review_text, review_in_english, review_text_eng, has_host_response, host_reply_text, stars_accuracy, stars_communication, stars_location, stars_checkin, stars_cleanliness, stars_value) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+var reviewsParams = ['reviewer_id', 'room_id', 'review_date', 'review_text', 'review_is_english', 'review_text_eng', 'has_host_response', 'host_reply_text', 'stars_accuracy', 'stars_communication', 'stars_location', 'stars_checkin', 'stars_cleanliness', 'stars_value'];
 
 ////////Helper Functions///////
 
@@ -32,8 +32,8 @@ const createUsersTableData = () => {
   var dummyData = [];
   for (var i = 0; i < NUM_OF_USERS; i++) {
     dummyData.push({ 
-      firstName: faker.name.firstName(), 
-      lastName: faker.name.lastName(), 
+      first_name: faker.name.firstName(), 
+      last_name: faker.name.lastName(), 
       image_photo_path: faker.image.avatar(),
     });
   }
@@ -59,9 +59,9 @@ var createReviewsTableData = () => {
       room_id: getRandomInt(NUM_OF_ROOMS),
       review_date: faker.date.past(),
       review_text: faker.lorem.paragraph(),
-      reviewIsEnglish: getRandomBoolean(),
+      review_in_english: getRandomBoolean(),
       review_text_eng: faker.lorem.paragraph(), 
-      hasHostResponse: getRandomBoolean(),
+      has_host_response: getRandomBoolean(),
       host_reply_text: faker.lorem.paragraph(), 
       stars_accuracy: getRandomInt(5), 
       stars_communication: getRandomInt(5),
