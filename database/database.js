54ae63849ea976dc1f7cb6283445528f38786a4e
Ed Wu <ed.sh.wu@gmail.com>
2018-12-21 11:40:27 -0800

    complete getAverageStarsByRoomId database query func

--- database/database.js
+++ database/database.js
@@ -1,5 +1,4 @@
 var mysql = require('mysql');
-var dummydata = require('./dummydata.js')
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -9,61 +8,43 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-var seedUserTable = () => {
-  var usersDummyData = dummydata.createUsersTableData();
-  var queryStr = `INSERT INTO users (firstName, lastName, image_photo_path) VALUES (?, ?, ?);`
-  
-  usersDummyData.forEach((user) => {
-    var params = [user.firstName, user.lastName, user.image_photo_path];
-    connection.query(queryStr, params, (err, results) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('success', results)
-      }
-    })
+const getReviewsByRoomId = (roomid, callback) => {
+  var queryStr = `SELECT * FROM reviews WHERE room_id=${roomid}`;
+
+  // select u.firstName, u.lastName, r.review_text from users u, reviews r where r.room_id=2 AND r.reviewer_id=u.id;
+
+  connection.query(queryStr, (err, results) => {
+    if(err) {
+      console.log(err);
+      // callback(err)
+    } else {
+      console.log(results);
+      // callback(null, results);
+    }
   })
 }
 
-seedUserTable();
+const getReviewsByRoomIdAndQueryTerm = (roomid, queryTerm, callback) => {
 
-var seedRoomsTable = () => {
-  var usersDummyData = dummydata.createRoomsTableData();
-  var queryStr = `INSERT INTO rooms (name, host_id) VALUES (?, ?);`
-  
-  usersDummyData.forEach((room) => {
-    var params = [room.name, room.host_id];
-    connection.query(queryStr, params, (err, results) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('success', results)
-      }
-    })
+}
+
+const getAverageStarsByRoomId = (roomid, callback) => {
+  var queryStr = `SELECT AVG(stars_accuracy), AVG(stars_communication), 
+    AVG(stars_location), AVG(stars_checkin), AVG(stars_cleanliness), 
+    AVG(stars_value) FROM reviews WHERE room_id=${roomid}`;
+
+  connection.query(queryStr, (err, results) => {
+    if(err) {
+      console.log(err);
+      // callback(err)
+    } else {
+      console.log(results);
+      // callback(null, results);
+    }
   })
 }
 
-seedRoomsTable();
+// connection.end();
 
-var seedReviewsTable = () => {
-    var usersDummyData = dummydata.createReviewsTableData();
-    var queryStr = `INSERT INTO reviews (reviewer_id, room_id, review_date, review_text, reviewIsEnglish, review_text_eng, hasHostResponse, host_reply_text, stars_accuracy, stars_communication, stars_location, stars_checkin, stars_cleanliness, stars_value) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
-    
-    usersDummyData.forEach((review) => {
-      var params = [review.reviewer_id, review.room_id, review.review_date, review.review_text, review.reviewIsEnglish, review.review_text_eng, review.hasHostResponse, review.host_reply_text, review.stars_accuracy, review.stars_communication, review.stars_location, review.stars_checkin, review.stars_cleanliness, review.stars_value];
-    
-      connection.query(queryStr, params, (err, results) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('success', results)
-        }
-      })
-    })
-  }
+module.exports = connection;
 
-seedReviewsTable();
-
-module.exports = {
-  connection,
-}
