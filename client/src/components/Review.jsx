a485e0d3babb937b69a53681d54b62da79491164
Ed Wu <ed.sh.wu@gmail.com>
2018-12-25 11:33:16 -0800

    add rendering of avatar based on props

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Review = (props) => (
   <div>
-  <div className="review-avatar">avatar here</div>
+  <div className="review_image_photo_path">{props.review.image_photo_path}</div>
   <div className="review_name">{props.review.first_name}</div>
   <div className="review_date">{props.review.review_date}</div>
   <div className="review_text">{props.review.review_text}</div>
